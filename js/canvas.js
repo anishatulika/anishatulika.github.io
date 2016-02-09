af54a8ee32d8159dbdba6a8ab9449c7b26094b53
Anisha Tulika <anisha2604@gmail.com>
2016-02-09 17:33:54 +0530

    Website files added

---
+++ js/canvas.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  particleground(document.getElementById('header'), {
+    dotColor: 'rgba(255,255,255,0.25)',
+    lineColor: 'rgba(255,255,255,0.25)'
+  });
+});
