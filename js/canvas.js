04f73575924efc634466fc9da57a3346fc235874
Arnab Kundu <arnab.kundu@lowes.com>
2020-10-03 19:50:58 +0530

    init commit

--- js/canvas.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  particleground(document.getElementById('header'), {
-    dotColor: 'rgba(255,255,255,0.25)',
-    lineColor: 'rgba(255,255,255,0.25)'
-  });
-});
