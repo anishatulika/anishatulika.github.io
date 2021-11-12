0088961399da8b63fbd75e57eaaa307efc971f27
Arnab Kundu <arnabkund@gmail.com>
2021-11-12 19:25:04 +0530

    arnab website checkin 2

---
+++ js/canvas.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  $('#particle').particleground({
+    dotColor: 'rgba(255,255,255,0.25)',
+    lineColor: 'rgba(255,255,255,0.25)'
+  });
+});
